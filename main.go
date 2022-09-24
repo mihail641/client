91e220e4fd5f6713fc45b777439696377f4f6dcf
KateFox1982 <109987582+KateFox1982@users.noreply.github.com>
2022-09-24 09:48:57 +0400

    new commit

--- main.go
+++ main.go
@@ -9,34 +9,15 @@ import (
 	"net/http"
 )
 
-const (
-	URL = "http://127.0.0.1:4000/user"
-	URLGET="http://127.0.0.1:4000/users"
-
-
-)
-type App struct {
-	client *controller.Controller
-}
 
 func main() {
-	app := App{
-		client: controller.NewController(),
-	}
-	app.StartServer()
-}
-func (app *App) StartServer () {
 	router := mux.NewRouter()
 	router.HandleFunc("/do", func(res http.ResponseWriter, req *http.Request) {
 		//userCtrl := controller.NewUserCtrl()
 		con := controller.NewController()
 		con.HandleHttp(res, req)
 	}).Methods("GET")
-
-	//router.HandleFunc("/do", controller.HandleHttp).Methods("GET")
-	//log.Fatal(http.ListenAndServe(":5000", router))
-
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(":4000", router))
 	}
 
 
