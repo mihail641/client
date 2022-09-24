5b026849d6766f97379870c30212d92f83705a49
KateFox1982 <109987582+KateFox1982@users.noreply.github.com>
2022-09-24 21:00:00 +0400

    new commit

--- model/model.go
+++ model/model.go
@@ -5,46 +5,51 @@ import (
 	"fmt"
 )
 
-type Adapter struct {
-	model*adapter.Client
-
-}
-type User struct {
-	users *adapter.User
+type Model struct {
+	model *adapter.Client
 }
 
-func NewModel() *Adapter {
-	return &Adapter{
+func NewModel() *Model {
+	return &Model{
 		model: adapter.NewClient(),
 	}
-	
 }
-
-
-func (d*Adapter)ClientAlgorithmTake()([]User,error) {
-
-user, err := d.model.MakeRequestGet()
-if err != nil {
-	m := "Ошибка выполнеия 1 функции получения информации о всех пользователях"
-	fmt.Println(m, err)
-	return []User{},err
-}
-fmt.Println("Переданная структура", user)
-IdMin := d.model.Min(user)
-IdMax := d.model.Max(user)
-fmt.Println("Максимальное значение", IdMax)
-fmt.Println("Минимальное значение", IdMin)
-
-d.model.MakeRequestUpdate(IdMin)
-d.model.MakeRequestDelete(IdMax)
-d.model.MakeRequestCreate()
-t, err:=d.model.MakeRequestGet()
+func (d *Model) ClientAlgorithmTake() (adapter.User, error) {
+	user, err := d.model.MakeRequestGet()
+	if err != nil {
+		m := "Ошибка выполнеия 1 функции получения информации о всех пользователях"
+		fmt.Println(m, err)
+		return adapter.User{}, err
+	}
+	fmt.Println("Переданная структура", user)
+	IdMin := d.model.Min(user)
+	IdMax := d.model.Max(user)
+	fmt.Println("Максимальное значение", IdMax)
+	fmt.Println("Минимальное значение", IdMin)
+	_, err = d.model.MakeRequestUpdate(IdMin)
+	if err != nil {
+		m := "Ошибка выполнеия  функции изменения пользователя"
+		fmt.Println(m, err)
+		return adapter.User{}, err
+	}
+	_, err = d.model.MakeRequestDelete(IdMax)
+	if err != nil {
+		m := "Ошибка выполнеия  функции удаления пользователя"
+		fmt.Println(m, err)
+		return adapter.User{}, err
+	}
+	_, err = d.model.MakeRequestCreate()
+	if err != nil {
+		m := "Ошибка выполнеия  функции создания пользователя"
+		fmt.Println(m, err)
+		return adapter.User{}, err
+	}
+	t, err := d.model.MakeRequestGet()
 	if err != nil {
 		m := "Ошибка выполнеия 2 функции получения информации о всех пользователях: %s"
 		fmt.Println(m, err)
-		return []User{},err
+		return adapter.User{}, err
 	}
 	fmt.Println(t)
-
-return []User{}, nil
+	return adapter.User{}, nil
 }
