36719c0b937349805c0507f0baee04386344df0c
KateFox1982 <109987582+KateFox1982@users.noreply.github.com>
2022-10-23 17:33:03 +0400

    new commit

---
+++ adapter/iadapter.go
@@ -0,0 +1,16 @@
+package adapter
+
+type User struct {
+	ID   int    `xml:"id",json:"id"`
+	Name string `xml:"name",json:"name"`
+	Sale int    `xml:"sale",json:"sale"`
+}
+
+type IAdapter interface {
+	MakeRequestGet() ([]User, error)
+	MakeRequestCreate(user User) (User, error)
+	MakeRequestUpdate(idMin int, name string, sale int) (User, error)
+	MakeRequestDelete(idMax int) (User, error)
+	Max(p []User) int
+	Min(p []User) int
+}
