d7e1d8bfc6b9bb2940f8b22e508ee89f6722d522
KateFox1982 <109987582+KateFox1982@users.noreply.github.com>
2022-10-26 15:29:21 +0400

    new commit

--- main.go
+++ main.go
@@ -10,19 +10,22 @@ import (
 	"net/http"
 )
 
-var AdapterType string
-
 func main() {
-	flag.StringVar(&AdapterType, "AdapterType", adapter.FileAdapterType, adapter.DataBaseAdapterType)
+	var concreteAdapterType string
+	flag.StringVar(&concreteAdapterType, "concreteAdapterType", "adapter.File", "adapter.Db")
 	flag.Parse()
-
+	var p adapter.AdapterType
+	p = adapter.AdapterType(concreteAdapterType)
+	//adType:=make([]string,0)
+	//adType=append(adType, adapter.FileAdapterType)
+	//adType=append(adType,adapter.DataBaseAdapterType)
 	//запуск роутера
 	router := mux.NewRouter()
 	//router.HandleFunc регистрация первого маршрута, с URL оканчивающимся на "/users" и методом GET, созадет новый экземпляр конструктора
 	//контроллера с аргументом DB, прием-передача параметров функции контроллера Getusers
 	router.HandleFunc("/do", func(res http.ResponseWriter, req *http.Request) {
 		//userCtrl := controller.NewUserCtrl()
-		con := controller.NewController(AdapterType)
+		con := controller.NewController(p)
 		con.HandleHttp(res, req)
 	}).Methods("GET")
 	log.Fatal(http.ListenAndServe(":5000", router))
