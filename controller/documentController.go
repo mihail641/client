a3e7b2a0b5159c40a00e9abce0323edca814cbd1
KateFox1982 <109987582+KateFox1982@users.noreply.github.com>
2022-11-28 17:35:17 +0400

    new commit

---
+++ controller/documentController.go
@@ -0,0 +1,52 @@
+package controller
+
+import (
+	"fmt"
+	"strings"
+	//"log"
+	"net/http"
+)
+
+// Controller структура используется для конструктора контроллер
+type DocumentController struct {
+}
+
+// NewController конструктор контроллера, возращающий экземпляр структуры Controller
+
+func NewDocumentController() *DocumentController {
+	return &DocumentController{
+
+	}
+}
+func (d*DocumentController)GetSimpleTable(res http.ResponseWriter, req *http.Request)  {
+	fmt.Println("Читаю таблицу")
+tableHTML:=`<html lang="ru">
+	<table border="1" width="600">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<thead>
+	<tr>
+   <th>Колонка 1</th>
+   <th>Колонка 2</th>
+	</tr>
+	</thead>
+<tbody>
+		<tr>
+	<td>Значение 1</td>
+	<td>Значение 2</td>
+	</tr> <!--ряд с ячейками тела таблицы-->
+	<tr>
+	<td>Значение 3</td>
+	<td>Значение 4</td>
+	</tr> <!--ряд с ячейками тела таблицы-->
+</tbody>
+	</table>
+ </html>`
+	res.Header().Set("Content-Type", "text/html")
+stringSlice := []string{tableHTML}
+	stringByte := strings.Join(stringSlice, " ")
+	fmt.Println(stringByte)
+	html:=[]byte(stringByte)
+	fmt.Println(html)
+	res.Write(html)
+
+}
